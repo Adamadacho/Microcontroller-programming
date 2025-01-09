6604696665185fefe0f7f0a4737085e0a192be4c
Adam <adamkublinski@gmail.com>
2025-01-09 20:39:07 +0100

    MP_LAB1

---
+++ Program_LAB1-1/Program_LAB1-1.ino
@@ -0,0 +1,21 @@
+// Definicja pinów
+const int ledPin = 13;
+const int buttonPin = 2;
+void setup() {
+// Ustawienie pinu LED jako wyjście
+pinMode(ledPin, OUTPUT);
+// Ustawienie pinu przycisku jako wejście
+pinMode(buttonPin, INPUT);
+}
+void loop() {
+// Odczyt stanu przycisku
+int buttonState = digitalRead(buttonPin);
+// Sprawdzenie stanu przycisku
+if (buttonState == HIGH) {
+// Włączenie LED
+digitalWrite(ledPin, HIGH);
+} else {
+// Wyłączenie LED
+digitalWrite(ledPin, LOW);
+}
+}
